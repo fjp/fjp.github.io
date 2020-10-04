451e7d063b459bf0a497b3e35e3f88507dc9b76e
Franz Pucher <franz.pucher@gmail.com>
2020-10-04 11:34:01 +0200

    Revert "merge upstream template changes"
    This reverts commit f6c6c3a18c4538af2cc5fcef5eb38ac57f4cfed5, reversing
    changes made to 6fdf09aec654ac7774b89105903a7b32786a6854.

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -1,18 +1,16 @@
 /*
-GreedyNav.js - http://lukejacksonn.com/actuate
+GreedyNav.js - https://github.com/lukejacksonn/GreedyNav
 Licensed under the MIT license - http://opensource.org/licenses/MIT
 Copyright (c) 2015 Luke Jackson
 */
 
-$(function() {
-
+$(document).ready(function() {
   var $btn = $("nav.greedy-nav .greedy-nav__toggle");
   var $vlinks = $("nav.greedy-nav .visible-links");
   var $hlinks = $("nav.greedy-nav .hidden-links");
 
   var numOfItems = 0;
   var totalSpace = 0;
-  var closingTime = 1000;
   var breakWidths = [];
 
   // Get initial state
@@ -22,31 +20,38 @@ $(function() {
     breakWidths.push(totalSpace);
   });
 
-  var availableSpace, numOfVisibleItems, requiredSpace, timer;
+  var availableSpace, numOfVisibleItems, requiredSpace;
 
   function check() {
-
     // Get instant state
-    availableSpace = $vlinks.width() - 10;
+    availableSpace = $vlinks.width() - $btn.width();
     numOfVisibleItems = $vlinks.children().length;
     requiredSpace = breakWidths[numOfVisibleItems - 1];
 
-    // There is not enought space
+    // There is not enough space
     if (requiredSpace > availableSpace) {
-      $vlinks.children().last().prependTo($hlinks);
+      $vlinks
+        .children()
+        .last()
+        .prependTo($hlinks);
       numOfVisibleItems -= 1;
       check();
       // There is more than enough space
     } else if (availableSpace > breakWidths[numOfVisibleItems]) {
-      $hlinks.children().first().appendTo($vlinks);
+      $hlinks
+        .children()
+        .first()
+        .appendTo($vlinks);
       numOfVisibleItems += 1;
       check();
     }
     // Update the button accordingly
     $btn.attr("count", numOfItems - numOfVisibleItems);
     if (numOfVisibleItems === numOfItems) {
-      $btn.addClass('hidden');
-    } else $btn.removeClass('hidden');
+      $btn.addClass("hidden");
+    } else {
+      $btn.removeClass("hidden");
+    }
   }
 
   // Window listeners
@@ -54,21 +59,10 @@ $(function() {
     check();
   });
 
-  $btn.on('click', function() {
-    $hlinks.toggleClass('hidden');
-    clearTimeout(timer);
+  $btn.on("click", function() {
+    $hlinks.toggleClass("hidden");
+    $(this).toggleClass("close");
   });
 
-  $hlinks.on('mouseleave', function() {
-    // Mouse has left, start the timer
-    timer = setTimeout(function() {
-      $hlinks.addClass('hidden');
-    }, closingTime);
-  }).on('mouseenter', function() {
-    // Mouse is back, cancel the timer
-    clearTimeout(timer);
-  })
-
   check();
-
 });
